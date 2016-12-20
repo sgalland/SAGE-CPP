7cb0bc6cce6da4491e1705f8b97adb26dc6ff3a9
Sean Galland <s_galland@msn.com>
2016-12-20 16:29:26 -0700

    Added class for AgiPicture.

--- src/sage/interpreter/helpers/AgiColor.h
+++ src/sage/interpreter/helpers/AgiColor.h
@@ -14,15 +14,10 @@ protected:
 	uint8_t r;
 	uint8_t g;
 	uint8_t b;
-	//uint32_t rgb;
 	uint8_t dosColor;
-	
-	//uint32_t CreateRGBValue();
 public:
 	AgiColor();
 	AgiColor(uint8_t dosColor, std::string name, uint8_t r, uint8_t g, uint8_t b);
-
-	//uint32_t getRGBValue();
 
 	static const AgiColor getColorByDosColor(int dosColor);
 };
---
+++ src/sage/interpreter/resources/AgiPicture.h
@@ -0,0 +1,5 @@
+class AgiPicture
+{
+public:
+	AgiPicture();
+};
